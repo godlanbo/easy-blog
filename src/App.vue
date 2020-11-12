23dd2d8c66ee6dc26af7d9287a1212e3c34d07b0
God Lanbo <912300762@qq.com>
2020-11-12 13:17:43 +0800

    add --在mounted时请求全部所需数据，保存在store管理

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,13 @@ export default {
       { hid: 'keywords', name: 'keywords', content: '个人，博客' },
       { hid: 'description', name: 'description', content: '这是一个我的个人网站，用于放置我的一些生活上的趣事和一些学习路上的总结'}
     ]
+  },
+  async mounted() {
+    await this.$store.dispatch('getBlogsList')
+    await this.$store.dispatch('getLifeList')
+    this.$store.commit('setInfoStatus', true)
+    let e = new Event('img-ready')
+    window.dispatchEvent(e)
   }
 }
 </script>
