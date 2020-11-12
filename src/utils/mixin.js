1d9cf8150f0c6aa3a202be06b74e75a2b24f4028
God Lanbo <912300762@qq.com>
2020-11-12 13:20:13 +0800

    add --store数据混入

--- src/utils/mixin.js
+++ src/utils/mixin.js
@@ -1,50 +1,78 @@
-import { getBlogsList, getLifeList } from './../api/index';
+import { mapGetters } from 'vuex';
+// import { getBlogsList, getLifeList } from './../api/index';
 
-export const homeMixin = {
-  data() {
-    return {
-      blogsList: [],
-      lifeList: [],
-    }
-  },
-  async mounted() {
-    let resBlog = await getBlogsList()
-    let resLife = await getLifeList()
-    this.blogsList = resBlog.data.blogsList
-    this.lifeList = resLife.data.lifeList
-    // 数据加载完毕后去触发lazyImg收集懒加载图片对象
-    let e = new Event('img-ready')
-    window.dispatchEvent(e)
-  },
+// export const homeMixin = {
+//   data() {
+//     return {
+//       blogsList: [],
+//       lifeList: [],
+//     }
+//   },
+//   async mounted() {
+//     let resBlog = await getBlogsList()
+//     let resLife = await getLifeList()
+//     this.blogsList = resBlog.data.blogsList
+//     this.lifeList = resLife.data.lifeList
+//     // 数据加载完毕后去触发lazyImg收集懒加载图片对象
+//     let e = new Event('img-ready')
+//     window.dispatchEvent(e)
+//   },
+// }
+export const listInfoMixin = {
+  computed: {
+    ...mapGetters([
+      'blogsList',
+      'lifeList'
+    ])
+  }
 }
 
 export const lazyImgMixin = {
-  mounted() {
-    let observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(item => {
-        if (item.isIntersecting) {
-          let el = item.target
-          if (el.tagName === 'IMG') {
-            el.setAttribute('src', el.dataset.src)
-          } else {
-            el.style.backgroundImage = el.dataset.src
-          }
-          observer.unobserve(item.target)
-        }
-      })
-    })
-    window.addEventListener('img-ready', () => {
-      setTimeout(() => {
+  data() {
+    return {
+      lazyObserver: null
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'isLoadInfo'
+    ])
+  },
+  methods: {
+    initObserverImg() {
+      this.$nextTick(() => {
         let imgs = document.querySelectorAll('figure img')
         let bgs = document.querySelectorAll('.home-life-section-list-item')
         imgs.forEach(img => {
-          observer.observe(img)
+          this.lazyObserver.observe(img)
         })
         bgs.forEach(bg => {
-          observer.observe(bg)
+          this.lazyObserver.observe(bg)
         })
-      }, 1000)
-    })
+      })
+    }
+  },
+  mounted() {
+    if (!this.lazyObserver) {
+      this.lazyObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach(item => {
+          if (item.isIntersecting) {
+            let el = item.target
+            if (el.tagName === 'IMG') {
+              el.setAttribute('src', el.dataset.src)
+            } else {
+              el.style.backgroundImage = el.dataset.src
+            }
+            observer.unobserve(item.target)
+          }
+        })
+      })
+    }
+    if (!this.isLoadInfo) {
+      window.addEventListener('img-ready', this.initObserverImg)
+    } else {
+      this.initObserverImg()
+    }
   },
 }
 
