e9c0dfa767144f4ccc74d2cc93ac7fb22fb58f9a
God Lanbo <912300762@qq.com>
2020-11-12 13:19:03 +0800

    add --将数据移动到store管理，这些数据多页面会用到

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,39 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { getBlogsList, getLifeList } from '../api'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  
+  state: {
+    blogsList: [],
+    lifeList: [],
+    isLoadInfo: false
+  },
+  actions: {
+    async getBlogsList({ commit }) {
+      let resBlog = await getBlogsList()
+      return commit('setBlogsList', resBlog.data.blogsList)
+    },
+    async getLifeList({ commit }) {
+      let resLife = await getLifeList()
+      return commit('setLifeList', resLife.data.lifeList)
+    }
+  },
+  mutations: {
+    setBlogsList(state, list) {
+      state.blogsList = list
+    },
+    setLifeList(state, list) {
+      state.lifeList = list
+    },
+    setInfoStatus(state, status) {
+      state.isLoadInfo = status
+    }
+  },
+  getters: {
+    blogsList: state => state.blogsList,
+    lifeList: state => state.lifeList,
+    isLoadInfo: state => state.isLoadInfo
+  }
 })
