6cce163917f00f0b006c99c56856476e82c7f8a5
God Lanbo <912300762@qq.com>
2020-11-18 12:30:52 +0800

    add --增加验证邮箱函数

--- src/utils/index.js
+++ src/utils/index.js
@@ -61,3 +61,17 @@ export function ensurePageLoaded(cb) {
     }
   }, 50)
 }
+
+export function isEmail(val) {
+  var pattern = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+  var domains = ["qq.com", "163.com", "vip.163.com", "263.net", "yeah.net", "sohu.com", "sina.cn", "sina.com", "eyou.com", "gmail.com", "hotmail.com", "42du.cn"];
+  if (pattern.test(val)) {
+    var domain = val.substring(val.indexOf("@") + 1);
+    for (var i = 0; i < domains.length; i++) {
+      if (domain === domains[i]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
