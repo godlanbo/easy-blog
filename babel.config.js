9660df40f731de96653965785ab203b01834606c
God Lanbo <912300762@qq.com>
2020-11-27 19:04:00 +0800

    add --添加prism插件，按需引入

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  plugins: [
+    [
+      "prismjs",
+      {
+        "languages": ["html", "css", "javascript", "typescript", "http", "scss", "nginx", "sql"],
+        "theme": "okaidia",
+        "css": true
+      }
+    ]
   ]
 }
