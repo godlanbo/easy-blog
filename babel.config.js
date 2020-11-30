11626d6a4c3b731b6bc51353ff632671ed217854
God Lanbo <912300762@qq.com>
2020-11-30 13:03:07 +0800

    add --更多代码段语言支持

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,7 @@ module.exports = {
     [
       "prismjs",
       {
-        "languages": ["html", "css", "javascript", "typescript", "http", "scss", "nginx", "sql"],
+        "languages": ["html", "css", "javascript", "typescript", "http", "scss", "nginx", "sql", "markup"],
         "theme": "okaidia",
         "css": true
       }
