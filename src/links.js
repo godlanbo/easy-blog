f1d5d59ca84b3a2c9d509343aca03f8cff65a611
godlanbo <912300762@qq.com>
2020-12-31 18:29:34 +0800

    add --友联资源文件

---
+++ src/links.js
@@ -0,0 +1,14 @@
+const links = [
+  {
+    name: 'FrederChen的个人博客',
+    link: 'https://frederchen.com/',
+    coverUrl: 'http://q1.qlogo.cn/g?b=qq&nk=850057322&s=5'
+  },
+  {
+    name: 'Colin’s Blog',
+    link: 'http://blog.colinx.one/',
+    coverUrl: 'http://q1.qlogo.cn/g?b=qq&nk=1580440944&s=5'
+  }
+]
+
+export default links
