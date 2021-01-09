ed6a5bee3fc560edcb95051b4b61267a8e15456a
godlanbo <912300762@qq.com>
2021-01-09 15:58:38 +0800

    feat --引入tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      '2xl': { max: '1535px' },
+      // => @media (max-width: 1535px) { ... }
+      xl: { max: '1279px' },
+      // => @media (max-width: 1279px) { ... }
+      lg: { max: '1023px' },
+      // => @media (max-width: 1023px) { ... }
+      md: { max: '768px' },
+      // => @media (max-width: 767px) { ... }
+      sm: { max: '639px' }
+      // => @media (max-width: 639px) { ... }
+    },
+    fontFamily: {},
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
