24518bb3ef9049575ebf1d64afef10fe1e2d5af5
godlanbo <912300762@qq.com>
2021-01-10 13:54:06 +0800

    update --tailwind 配置颜色等配置项

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,23 @@
+const defaultColor = require('./color')
+const customColor = {
+  white: {
+    default: '#fff',
+    primary: '#f0f3f6'
+  },
+  black: '#000',
+  transparent: 'transparent',
+  current: 'currentColor',
+  theme: {
+    text: '#2c3f51',
+    primary: '#30849a',
+    menu: '#082634'
+  }
+}
+const colors = Object.assign(defaultColor, customColor)
+
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     screens: {
       '2xl': { max: '1535px' },
@@ -14,8 +31,15 @@ module.exports = {
       sm: { max: '639px' }
       // => @media (max-width: 639px) { ... }
     },
-    fontFamily: {},
-    extend: {}
+    colors,
+    extend: {
+      transitionDuration: {
+        primary: '300ms'
+      },
+      transitionTimingFunction: {
+        primary: 'ease'
+      }
+    }
   },
   variants: {
     extend: {}
