d3c4234883187aec7a1bbc43e28b2885a780227e
godlanbo <912300762@qq.com>
2021-02-04 15:14:26 +0800

    fix --修复当进入博客时没有hash值获取锚点报错 现在只有当有hash值的时候才会尝试初始化锚点

--- src/utils/initHashLocation.js
+++ src/utils/initHashLocation.js
@@ -1,7 +1,9 @@
 // 初始化锚点
 export default function initHashLocation() {
   let hash = location.hash
-  let decodeHash = decodeURIComponent(hash)
-  let targetScrollTop = document.querySelector(decodeHash).offsetTop
-  window.scrollTo(0, targetScrollTop)
+  if (hash.length > 0) {
+    let decodeHash = decodeURIComponent(hash)
+    let targetScrollTop = document.querySelector(decodeHash).offsetTop
+    window.scrollTo(0, targetScrollTop)
+  }
 }
