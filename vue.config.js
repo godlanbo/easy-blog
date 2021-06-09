81d5dfa160569cedbad018828bd68383bd4803a0
godlanbo <912300762@qq.com>
2021-06-09 21:43:29 +0800

    fix --配置文件修改，现在本地开发会读取本地依赖包，不会走CDN

--- vue.config.js
+++ vue.config.js
@@ -8,32 +8,43 @@ function resolve(dir) {
 
 let vueCdn = process.env.NODE_ENV === 'production' ? 'vue.min.js' : 'vue.js'
 
+let configureWebpack = {
+  // provide the app's title in webpack's name field, so that
+  // it can be accessed in index.html to inject the correct title.
+  resolve: {
+    alias: {
+      '@': resolve('src')
+    }
+  }
+}
+
+if (process.env.NODE_ENV === 'production') {
+  configureWebpack['externals'] = {
+    vue: 'Vue',
+    'element-ui': 'ELEMENT'
+  }
+}
+
 module.exports = {
   productionSourceMap: process.env.NODE_ENV === 'production' ? false : true,
-  configureWebpack: {
-    // provide the app's title in webpack's name field, so that
-    // it can be accessed in index.html to inject the correct title.
-    resolve: {
-      alias: {
-        '@': resolve('src')
-      }
-    },
-    externals: {
-      vue: 'Vue',
-      'element-ui': 'ELEMENT'
-    }
-  },
+  configureWebpack,
   chainWebpack(config) {
-    const cdn = {
-      css: [
+    let css = [],
+      js = []
+    if (process.env.NODE_ENV === 'production') {
+      css = [
         // element-ui css
         'https://cdn.jsdelivr.net/npm/element-ui@2.14.1/lib/theme-chalk/index.css'
-      ],
-      js: [
+      ]
+      js = [
         `https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/${vueCdn}`,
         // element-ui js
         'https://cdn.jsdelivr.net/npm/element-ui@2.14.1/lib/index.js'
       ]
+    }
+    const cdn = {
+      css,
+      js
     }
     config.plugin('html').tap(args => {
       args[0].cdn = cdn
