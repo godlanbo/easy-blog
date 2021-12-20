4b68ba578664831afa2aae67ec7ca282b49e5906
godlanbo <912300762@qq.com>
2021-12-20 21:44:21 +0800

    fix: 修复同一个url复数出现时，懒加载出错

--- src/directives/lazyLoadImg.js
+++ src/directives/lazyLoadImg.js
@@ -1,9 +1,10 @@
 import defaultImg from '../../public/image/default.png'
 
 const imgState = {
-  loading: 0,
-  loaded: 1,
-  error: 2
+  wait: 0,
+  loading: 1,
+  loaded: 2,
+  error: 3
 }
 
 const imgResSrc = /^(data|https?)/
@@ -27,43 +28,63 @@ function loadImage(src) {
 }
 
 class ImageLoader {
-  constructor(el, src, loadingSrc, errorSrc, type) {
-    this.state = imgState.loading
-    // 标识唯一的资源
-    el.setAttribute('data-lazy-key', src)
+  constructor(src, loadingSrc, errorSrc) {
+    this.state = imgState.wait
     this.src = src
     this.errSrc = errorSrc
-    this.el = el
-    this.type = type || 'img' // img | bg img元素和背景图片
-    this.setSrc(loadingSrc)
+    this.loadingSrc = loadingSrc
+    // 可能有多个图片用同一个url，loader把他们都收集起来，只需要加载一次
+    this.elements = []
+  }
+  addImage(el, type) {
+    el.src = this.loadingSrc
+    // 标识唯一的资源
+    el.setAttribute('data-lazy-key', this.src)
+    // img | bg img元素和背景图片
+    this.elements.push({
+      el,
+      type: type || 'img'
+    })
+    ImageLoader.setSrc(el, type, this.loadingSrc)
   }
   renderImage() {
-    if (this.state === imgState.loaded || this.state === imgState.error) {
+    if (
+      this.state === imgState.loaded ||
+      this.state === imgState.error ||
+      this.state === imgState.loading
+    ) {
       return
     }
+    this.state = imgState.loading
     loadImage(this.src)
       .then(() => {
         this.state = imgState.loaded
-        this.setSrc(this.src)
+        this.walkImage(this.src)
       })
       .catch(err => {
         this.state = imgState.error
-        this.setSrc(this.errSrc)
+        this.walkImage(this.errSrc)
         console.error(
           `Image: ${this.src} load error, the error msg is: ${err.message}`
         )
       })
   }
   // 将加载好的资源装载在元素上
-  setSrc(src) {
-    if (this.type === 'bg') {
+  static setSrc(el, type, src) {
+    if (type === 'bg') {
       if (src.match(imgResSrc)) {
-        this.el.style.backgroundImage = `url(${src})`
+        el.style.backgroundImage = `url(${src})`
       } else {
-        this.el.style.backgroundImage = src
+        el.style.backgroundImage = src
       }
-    } else if (this.type === 'img') {
-      this.el.src = src
+    } else if (type === 'img') {
+      el.src = src
+    }
+  }
+  // 遍历所有元素，分别设置他们的src
+  walkImage(src) {
+    for (let i = 0; i < this.elements.length; i++) {
+      ImageLoader.setSrc(this.elements[i].el, this.elements[i].type, src)
     }
   }
 }
@@ -76,16 +97,15 @@ class LazyLoadManager {
     this.collectMap = new Map()
     this.observer = this.initObserver()
   }
-  addImage(el, src, type) {
-    const imgLoader = new ImageLoader(
-      el,
-      src,
-      this.loadingSrc,
-      this.errorSrc,
-      type
-    )
-    this.observer.observe(el)
+  collectImage(el, src, type) {
+    let imgLoader
+    if (this.collectMap.has(src)) {
+      imgLoader = this.collectMap.get(src)
+    }
+    imgLoader = new ImageLoader(src, this.loadingSrc, this.errorSrc)
+    imgLoader.addImage(el, type)
     this.collectMap.set(src, imgLoader)
+    this.observer.observe(el)
   }
   initObserver() {
     return new IntersectionObserver(entries => {
@@ -94,8 +114,10 @@ class LazyLoadManager {
           const target = entry.target
           const lazyKey = target.dataset.lazyKey
           const lazyImageLoader = this.collectMap.get(lazyKey)
-          if (lazyImageLoader.state === imgState.loading) {
+          if (lazyImageLoader.state === imgState.wait) {
             lazyImageLoader.renderImage()
+          } else if (lazyImageLoader.state === imgState.loading) {
+            this.observer.unobserve(target)
           } else {
             this.collectMap.delete(lazyKey)
             this.observer.unobserve(target)
@@ -113,7 +135,7 @@ const LazyLoadImg = {
     })
     app.directive('lazy', {
       bind(el, binding) {
-        lazyLoadInstance.addImage(el, binding.value, binding.arg)
+        lazyLoadInstance.collectImage(el, binding.value, binding.arg)
       }
     })
   }
--- src/links.js
+++ src/links.js
@@ -2,12 +2,12 @@ const links = [
   {
     name: 'FrederChen的个人博客',
     link: 'https://frederchen.com/',
-    coverUrl: 'http://q1.qlogo.cn/g?b=qq&nk=850057322&s=5'
+    coverUrl: 'https://q1.qlogo.cn/g?b=qq&nk=850057322&s=5'
   },
   {
     name: 'Colin’s Blog',
     link: 'http://blog.colinx.one/',
-    coverUrl: 'http://q1.qlogo.cn/g?b=qq&nk=1580440944&s=5'
+    coverUrl: 'https://q1.qlogo.cn/g?b=qq&nk=1580440944&s=5'
   }
 ]
 
